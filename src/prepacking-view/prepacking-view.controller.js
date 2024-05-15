f1b457b55cd7a8993e2668eb660880e00559891a
Mohau Nkepane <mohaunkepane@gmail.com>
2024-05-15 08:54:57 +0200

    Fixed Prepack View table  Caption

--- src/prepacking-view/prepacking-view.controller.js
+++ src/prepacking-view/prepacking-view.controller.js
@@ -38,6 +38,7 @@
         vm.getFacilityName = getFacilityName;
         vm.getProgramName = getProgramName;
         vm.filterPrepacksForAuthorisation = filterPrepacksForAuthorisation;
+        vm.prepackDetailsLength = 0;
         
         function onInit(){
             vm.facility = facility;            
@@ -45,6 +46,7 @@
             vm.programs = programs;
             vm.prepackDetails = (prepackStage === "authorise") ? vm.filterPrepacksForAuthorisation(Prepacks) : Prepacks;            
             formatPrepacks();
+            vm.prepackDetailsLength = Object.keys(vm.prepackDetails).length;
         }
         onInit();
 
