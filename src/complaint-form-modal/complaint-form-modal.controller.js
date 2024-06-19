62fdc0502f08220e580919055c49d7d0bc516213
mmokone <sallymokone@gmail.com>
2024-06-19 16:48:01 +0200

    Complaint form almost done

--- src/complaint-form-modal/complaint-form-modal.controller.js
+++ src/complaint-form-modal/complaint-form-modal.controller.js
@@ -29,38 +29,42 @@
         .controller('complaintFormModalController', controller);
 
     controller.$inject = [ 'modalDeferred', '$scope', 'rejectionReasons', 'itemTimestamp', 'stockAdjustmentCreationService', 'notificationService', 
-        'orderableGroups', 'program', 'facility', 'programService', 'orderableGroupService', 'hasPermissionToAddNewLot'];//
+        'orderableGroups', 'program', 'facility', 'programService', 'orderableGroupService', 'hasPermissionToAddNewLot', 'messageService'];
 
     function controller( modalDeferred, $scope, rejectionReasons, itemTimestamp, stockAdjustmentCreationService, 
-                        notificationService, orderableGroups, program, faciity, programService, orderableGroupService, hasPermissionToAddNewLot) {//
+                        notificationService, orderableGroups, program, faciity, programService, orderableGroupService, hasPermissionToAddNewLot, messageService) {//
         var vm = this;
 
         vm.$onInit = onInit;
         vm.confirm = confirm;
+        vm.lotChanged = lotChanged;
+        vm.addProductToComplaintForm = addProductToComplaintForm;
+        vm.removeProductLineItem = removeProductLineItem;
+        vm.productsForComplaint = [];
         //vm.discrepancies = rejectionReasons;
         vm.discrepancyOptions = [];
         vm.discrepancies =[];
         vm.selectedDiscrepancy = undefined;
-        vm.addDiscrepancy = addDiscrepancy;
-        vm.removeDispency = removeDiscrepancy;
+        //vm.addDiscrepancy = addDiscrepancy;
+        //vm.removeDispency = removeDiscrepancy;
 
         $scope.showModal=false;
         
 
-        // adding discrepancies to table
-        function addDiscrepancy() {
-            vm.discrepancies.push({
-                'name': vm.selectedDiscrepancy,
-                'quantity': '',
-                'comments': ''
-            });
-            console.log(vm.discrepancies);
-        };
+        // // adding productsForComplaint to table
+        // function addDiscrepancy() {
+        //     vm.productsForComplaint.push({
+        //         'name': vm.vm.selectedOrderableGroup,
+        //         'quantity': '',
+        //         'comments': ''
+        //     });
+        //     console.log(vm.productsForComplaint);
+        // };
 
-        // removing discrepancies from table
-        function removeDiscrepancy(index) {
-            vm.discrepancies.splice(index, 1);
-        }
+        // // removing productsForComplaint from table
+        // function removeDiscrepancy(index) {
+        //     vm.productsForComplaint.splice(index, 1);
+        // }
         
         function onInit() {
             vm.selectedDiscrepancy = [];
@@ -86,6 +90,81 @@
            vm.showVVMStatusColumn = orderableGroupService.areOrderablesUseVvm(
              vm.orderableGroups
            );
+        }
+
+        /**
+         * @ngdoc method
+         * @methodOf stock-adjustment-creation.controller:StockAdjustmentCreationController
+         * @name orderableSelectionChanged
+         *
+         * @description
+         * Reset form status and change content inside lots drop down list.
+         */
+        vm.orderableSelectionChanged = function () {
+            //reset selected lot, so that lot field has no default value
+            vm.selectedLot = null;
+    
+            initiateNewLotObject();
+            vm.canAddNewLot = false;
+    
+            //same as above
+            $scope.productForm.$setUntouched();
+    
+            //make form good as new, so errors won't persist
+            $scope.productForm.$setPristine();
+    
+            vm.lots = orderableGroupService.lotsOf(
+            vm.selectedOrderableGroup,
+            vm.hasPermissionToAddNewLot
+            );
+            vm.selectedOrderableHasLots = vm.lots.length > 0;
+        };
+
+        /**
+         * @ngdoc method
+         * @methodOf stock-adjustment-creation.controller:StockAdjustmentCreationController
+         * @name lotChanged
+         *
+         * @description
+         * Allows inputs to add missing lot to be displayed.
+         */
+        function lotChanged() {
+            vm.canAddNewLot =
+            vm.selectedLot &&
+            vm.selectedLot.lotCode ===
+                messageService.get('orderableGroupService.addMissingLot');
+            initiateNewLotObject();
+        }
+
+        function initiateNewLotObject() {
+            vm.newLot = {
+              active: true,
+            };
+        }
+
+        function addProductToComplaintForm() {
+            console.log("Adding products...");
+            console.log(vm.selectedOrderableGroup);
+            console.log(vm.selectedOrderableGroup[0].orderable.fullProductName);
+            console.log("Lot...");
+            console.log(vm.selectedLot.expirationDate);
+            // if(vm.selectedDiscrepancy.length!=0){
+                vm.productsForComplaint.push({
+                    'name': vm.selectedOrderableGroup[0].orderable.fullProductName,
+                    'batch': vm.selectedLot.lotCode,
+                    'expiary': vm.selectedLot.expirationDate
+                    // 'quantityReturned': '',
+                    // 'natureOfComplaint': ''
+                });  
+            // }
+            // else{
+            //     notificationService.error('Select a discrepancy before adding.');
+            // }
+        }
+
+        // removing discrepancies from table
+        function removeProductLineItem(index) {
+            vm.productsForComplaint.splice(index, 1);
         }
 
         //builds receiving payload
